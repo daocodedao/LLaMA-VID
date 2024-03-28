ec7498bcf5a64e392d899d691d975037a5343c9f
linzhiji <linzhiji@gmail.com>
2024-03-28 18:44:54 +0800

    fix bug

--- test-video-client.py
+++ test-video-client.py
@@ -2,7 +2,7 @@
 import requests
 
 
-url = "http://127.0.0.1:9890/video/describe/"
+url = "http://localhost:9890/video/describe/"
 videoPath = "./demos/video1.0.mp4"
 files = {'upload_file': open(videoPath,'rb')}
 
