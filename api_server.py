276bfccbc1cc951c59f9626cd760d3abdaaafb03
linzhiji <linzhiji@gmail.com>
2024-03-28 19:17:43 +0800

    fix bug

--- api_server.py
+++ api_server.py
@@ -14,13 +14,26 @@ api_logger.info("加载模型-完毕")
 app = FastAPI()
 @app.post("/video/describe/")
 async def upload_file(file: UploadFile = File(...)):
-    if not file.endswith(".mp4"):
-        return {"code":201, "message":"Need upload video file"} 
-    api_logger.info(f"收到文件：{file.filename}")
     saveVideoName = f"{Util.getCurTimeStampStr()}.mp4"
     videoPath = os.path.join("/tmp/", saveVideoName)
-    with open(videoPath, "wb") as f:
-        f.write(file.file.read())
+
+    try:
+        api_logger.info(f"收到文件：{file.filename}")
+        contents = file.file.read()
+        with open(videoPath, 'wb') as f:
+            f.write(contents)
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+    finally:
+        file.file.close()
+
+
+    # if not file.endswith(".mp4"):
+    #     return {"code":201, "message":"Need upload video file"} 
+    # api_logger.info(f"收到文件：{file.filename}")
+
+    # with open(videoPath, "wb") as f:
+    #     f.write(file.file.read())
 
     desc = describeVideo(videoPath)
     api_logger.info(f"视频描述：{file.filename}")
