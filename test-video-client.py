ecfe4156ad0764e5bed8a397523ce610b49ec947
linzhiji <linzhiji@gmail.com>
2024-03-28 18:55:33 +0800

    fix bug

--- test-video-client.py
+++ test-video-client.py
@@ -5,7 +5,8 @@ import requests
 # curl -X POST http://localhost:9890/uploadfile/ -F "file=@my_video.mp4"
 
 
-url = "http://192.168.0.65:9890/video/describe/"
+# url = "http://192.168.0.65:9890/video/describe/"
+url = "http://127.0.0.1/video/describe/"
 videoPath = "./demos/video1.0.mp4"
 files = {'upload_file': open(videoPath,'rb')}
 
