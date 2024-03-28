8b33bce478d46d6873d83743b17a94cfc2b70562
linzhiji <linzhiji@gmail.com>
2024-03-28 18:22:58 +0800

    fix bug

--- test-des-video.py
+++ test-des-video.py
@@ -1,10 +1,10 @@
 from utilDescribeVideo import *
 import os
-
+from utils.logger_settings import api_logger
 
 
 initModel()
 videoPath = "demos/video1.0.mp4"
 desc = describeVideo(videoPath)
-
-print(desc)
+api_logger.info("视频描述：")
+api_logger.info(desc)
