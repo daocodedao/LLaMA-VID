7e964a865b4487bb624d0deed4b00aba1a9fe9fd
linzhiji <linzhiji@gmail.com>
2024-03-28 19:18:45 +0800

    fix bug

--- test-video-client.py
+++ test-video-client.py
@@ -8,9 +8,10 @@ import requests
 url = "http://localhost:9890/video/describe/"
 # url = "http://127.0.0.1/video/describe/"
 videoPath = "./demos/video1.0.mp4"
-files = {'upload_file': open(videoPath,'rb')}
+# files = {'upload_file': open(videoPath,'rb')}
+file = {'file': open(videoPath, 'rb')}
 
-response = requests.post(url, files=files)
+response = requests.post(url, files=file)
 retJson = response.json()
 # ret_text = retJson["message"]
 
