aa659b3870be1828e6b6fdb2e1d42825b049959d
linzhiji <linzhiji@gmail.com>
2024-03-28 17:48:05 +0800

    fix bug

--- test-des-video.py
+++ test-des-video.py
@@ -2,7 +2,7 @@ from utilDescribeVideo import *
 import os
 
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
-os.environ["CUDA_VISIBLE_DEVICES"]="0"
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
 initModel()
 videoPath = "demos/video1.0.mp4"
