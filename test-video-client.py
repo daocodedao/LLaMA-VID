12194c16fd8613cb7f39678344d25a4934b4d13a
linzhiji <linzhiji@gmail.com>
2024-03-29 10:47:07 +0800

    split

--- test-video-client.py
+++ test-video-client.py
@@ -5,9 +5,14 @@ from utils.util import Util
 # filePath = Util.getTempMp4FilePath()
 videoPath = "./demos/video1.0.mp4"
 
-url = 'http://localhost:9890/video/describe'
+url = 'http://39.105.194.16:9690/video/describe'
+# url = 'http://localhost:9690/video/describe'
+
 file = {'file': open(videoPath, 'rb')}
 resp = requests.post(url=url, files=file) 
-print(resp.json())
+result = resp.json()
 
-exit()
+print(resp.json())
+print(result['message'])
+
+
